1c052342b250349b197be1f6ee9cde34b1593835
JtheDroid <fabi.jobo@gmail.com>
2020-10-28 19:48:37 +0100

    Modify reply format, limit message length
    reduce newlines in output
    cut off messages at discords limit (2000 characters)
    
    package-lock.json: update name

--- index.js
+++ index.js
@@ -10,28 +10,40 @@ async function exec_command(command, args = null, directory = null) {
     const command_args = args ? args : command.trim().split(/ +/g);
     if (!args) command = command_args.shift()
     const p = childProcess.spawnSync(command, command_args, options);
-    let output = ""
+    let output = "";
     if (p.stdout && p.stdout.length > 0)
-        output += p.stdout;
-    if (p.stderr && p.stderr.length > 0)
-        output += `\nstderr:\n${p.stderr}`;
-    if (p.error)
-        output += `\nerror:\n${p.error}`;
-    return output
+        output += p.stdout
+    if (p.stderr && p.stderr.length > 0) {
+        if (output.length > 0) output += "\n";
+        output += p.stderr;
+    }
+    if (p.error) {
+        if (output.length > 0) output += "\n";
+        output += `error:\n${p.error}`;
+    }
+    return output.trim();
 }
 
 async function execute_commands(command_list, command_name) {
-    let command_output = `${command_name}:\n`
+    let command_output = `\`${command_name}\`:\n`;
     for (const command of command_list[command_name]) {
         const command_dir = command.hasOwnProperty("dir") ? command.dir : null;
         const command_command = command.hasOwnProperty("command") ? command.command : null;
-        const command_args = command.hasOwnProperty("args") ? command.args : null
+        const command_args = command.hasOwnProperty("args") ? command.args : null;
         if (command) {
-            command_output += "\n"
-            command_output += await exec_command(command_command, command_args, command_dir)
+            command_output += await exec_command(command_command, command_args, command_dir);
+            command_output += "\n";
         }
     }
-    return command_output
+    return command_output;
+}
+
+async function extend_message(message, text) {
+    let new_text = `${message.content}\n${text}`;
+    if (new_text.length > 2000) {
+        new_text = new_text.substring(0, 2000);
+    }
+    return message.edit(new_text);
 }
 
 client.on('ready', () => {
@@ -48,11 +60,11 @@ client.on('message', async msg => {
     const command = args.shift().toLowerCase();
     if (command === "service") {
         if (args.length < 1) {
-            await msg.reply("usage: +service <servicename> action(s)")
+            await msg.reply("usage: +service <servicename> action(s)");
             return;
         }
         if (!services.hasOwnProperty(author_id)) {
-            await msg.reply("not authorized!")
+            await msg.reply("not authorized!");
             return;
         }
         const user_services = services[author_id];
@@ -61,19 +73,25 @@ client.on('message', async msg => {
             await msg.reply("service not found");
             return;
         }
-        const service_commands = user_services[service_name]
+        const service_commands = user_services[service_name];
         if (args.length === 0) {
             await msg.reply(`actions for ${service_name}: ${Object.getOwnPropertyNames(service_commands).join(", ")}`);
             return;
         }
-        let reply_msg = await msg.reply(`service ${service_name}`);
+        let reply_msg = await msg.reply(`**${service_name}**`);
         while (args.length > 0) {
             const command = args.shift();
             if (service_commands.hasOwnProperty(command)) {
-                const command_output = await execute_commands(service_commands, command, x => reply_msg.edit(`${reply_msg.content}\n${x}`));
-                reply_msg = await reply_msg.edit(`${reply_msg.content}\n\n${command_output}`);
+                const command_output = await execute_commands(service_commands, command);
+                reply_msg = await extend_message(reply_msg, command_output).catch(reason => {
+                    console.error(reason);
+                    return reply_msg;
+                });
             } else {
-                reply_msg = await reply_msg.edit(`${reply_msg.content}\n\ncommand ${command} not found`);
+                reply_msg = await extend_message(reply_msg, `\ncommand \`${command}\` not found`).catch(reason => {
+                    console.error(reason);
+                    return reply_msg;
+                });
             }
         }
     } else if (command === "services") {
@@ -82,7 +100,7 @@ client.on('message', async msg => {
             return;
         }
         const user_services = services[author_id];
-        await msg.reply(`services: ${Object.getOwnPropertyNames(user_services).join(", ")}`)
+        await msg.reply(`services: ${Object.getOwnPropertyNames(user_services).join(", ")}`);
     }
 
 });
